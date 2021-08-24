2d3d2e55da00f943b20031239de38ce46c2dc98f
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-24 17:29:26 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	int r = 0;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		// add the pages per day to 'r'
+		r += x;
+		// output the quotient of 'r' and 'm' (no. of pages if we divide 'r' names in 'm' pages)
+		cout << r / m << " ";
+		// calculate the remaining pages (if there are)
+		r %= m;
+	}
+	cout << '\n';
+	return 0;
+}
